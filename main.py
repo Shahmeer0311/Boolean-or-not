0047b113d0b280c5bc067ea0cf503ff3c4e37a86
Shahmeer0311 <22alis@students.watfordboys.org>
2024-12-07 14:23:42 +0000

    BooleanOrNot

---
+++ main.py
@@ -0,0 +1,19 @@
+a = 0
+b = 0
+c = 0
+if not a and b and c:
+    print("All number have a boolean value of false")
+else:
+    print("Atleast one number has a boolean value of false")
+a = 10
+b = -10
+c = 0
+if  a > 0 or b > 0:
+    print("Atleast one of the numbers is greater than 0")
+else:
+    print("No number is greater than 0")
+
+if  b > 0 or c > 0:
+    print("Either of the numbers is greater than 0")
+else:
+    print("No number is greater than 0")
